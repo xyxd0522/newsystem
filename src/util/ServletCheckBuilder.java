67cd7f81e3aa85d0ddfd658335441b46d8be9e34
linglian <linglian@aliyun.com>
2018-01-13 12:54:23 +0800

    ServletUitl Finish

---
+++ src/util/ServletCheckBuilder.java
@@ -0,0 +1,11 @@
+package util;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletCheckBuilder {
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private HttpServlet servlet;
+}
